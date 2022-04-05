a57533f9790d7be0ef1c1d49aa8326b0456f8dd8
Pelikantapeten <mats.simonsson@sweora.se>
2022-04-05 08:49:44 +0000

    Add constant that contains a randomized order from the iconArray

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,28 +5,31 @@ document.addEventListener("DOMContentLoaded", () => {
 /**
  * Creates an array of 20 icons that will form the base of the deck
  */
-const cardDeck = [
-    'fa-solid fa-brackets-square',
+const iconArray = [
     'fa-solid fa-brackets-square',
     'fa-solid fa-brackets-round',
-    'fa-solid fa-brackets-round',
-    'fa-solid fa-brackets-curly',
     'fa-solid fa-brackets-curly',
     'fa-solid fa-code',
-    'fa-solid fa-code',
-    'fa-solid fa-bug',
     'fa-solid fa-bug',
     'fa-solid fa-code-simple',
-    'fa-solid fa-code-simple',
-    'fa-solid fa-code-branch',
     'fa-solid fa-code-branch',
     'fa-solid fa-code-fork',
-    'fa-solid fa-code-fork',
-    'fa-solid fa-bug-slash',
     'fa-solid fa-bug-slash',
     'fa-solid fa-laptop-code',
+    'fa-solid fa-brackets-square',
+    'fa-solid fa-brackets-round',
+    'fa-solid fa-brackets-curly',
+    'fa-solid fa-code',
+    'fa-solid fa-bug',
+    'fa-solid fa-code-simple',
+    'fa-solid fa-code-branch',
+    'fa-solid fa-code-fork',
+    'fa-solid fa-bug-slash',
     'fa-solid fa-laptop-code',
 ];
+
+// Randomizes the cards in the card array - learned how to do it at https://www.w3schools.com/js/js_array_sort.asp
+const cardDeck = iconArray.sort((a, b) => 0.5 - Math.random());
 
 console.log(cardDeck);
 
