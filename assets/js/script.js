5ab73a2970b3b4c6e41dd6d71bbe187a183fba1f
Pelikantapeten <mats.simonsson@sweora.se>
2022-04-05 07:53:16 +0000

    Add array of icons that will form deck of card in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Finsih loading the DOM before game can start
+
+document.addEventListener("DOMContentLoaded", () => {
+
+/**
+ * Creates an array of 20 icons that will form the base of the deck
+ */
+const cardDeck = [
+    'fa-solid fa-brackets-square',
+    'fa-solid fa-brackets-square',
+    'fa-solid fa-brackets-round',
+    'fa-solid fa-brackets-round',
+    'fa-solid fa-brackets-curly',
+    'fa-solid fa-brackets-curly',
+    'fa-solid fa-code',
+    'fa-solid fa-code',
+    'fa-solid fa-bug',
+    'fa-solid fa-bug',
+    'fa-solid fa-code-simple',
+    'fa-solid fa-code-simple',
+    'fa-solid fa-code-branch',
+    'fa-solid fa-code-branch',
+    'fa-solid fa-code-fork',
+    'fa-solid fa-code-fork',
+    'fa-solid fa-bug-slash',
+    'fa-solid fa-bug-slash',
+    'fa-solid fa-laptop-code',
+    'fa-solid fa-laptop-code',
+];
+
+console.log(cardDeck);
+
+});
